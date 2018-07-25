57bd34b03ddde827e509dc5f47594787cafa7191
Thanh Tu Nguyen <tunt0168@gmail.com>
2018-07-25 18:47:20 +0700

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'tweetSimilarApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for tweetSimilarApp
+
+  target 'tweetSimilarAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'tweetSimilarAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  pod 'RATreeView', '~> 2.1'
+
+end
---
+++ tweetSimilarApp/ViewController/LoadingPageViewController/LoadingPageViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoadingPageViewController.swift
+//  tweetSimilarApp
+//
+//  Created by Thanh Tu Nguyen on 7/25/18.
+//  Copyright © 2018 Thanh Tu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class LoadingPageViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.isHidden = true
+        
+        UIApplication.shared.keyWindow?.windowLevel = UIWindowLevelStatusBar
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
